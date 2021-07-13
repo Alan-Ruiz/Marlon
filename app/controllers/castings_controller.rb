dd308b1cfa1f0a8fe190514ee6aea41a7111ac35
Alan-Ruiz <gheryk90@gmail.com>
2021-07-13 09:25:59 +0200

    casting controller create/new

--- app/controllers/castings_controller.rb
+++ app/controllers/castings_controller.rb
@@ -1,17 +1,33 @@
 class CastingsController < ApplicationController
+  before_action :set_casting, only: [:show]
 
   def index
     @castings = Casting.All
   end
 
   def show
-    @casting = Casting.find(params[:id])
   end
 
   def new
+    @casting = Casting.new
   end
 
   def create
+    @casting = Casting.new(casting_params)
+    if @casting.save
+      redirect_to castings_path
+    else
+      render :new
+    end
   end
 
+  private
+
+  def set_casting
+    @casting = Casting.find(params[:id])
+  end
+
+  def casting_params
+    params.require(:casting).permit(:title, :content, :city, :photo)
+  end
 end
