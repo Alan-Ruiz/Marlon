e67a2b25d61aac09d169ed15e4eeda32ef8f3e1e
Alan-Ruiz <gheryk90@gmail.com>
2021-07-16 17:48:16 +0200

    added fullname to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_122511) do
+ActiveRecord::Schema.define(version: 2021_07_16_154534) do
 
   create_table "castings", force: :cascade do |t|
     t.text "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_07_03_122511) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "fullname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
