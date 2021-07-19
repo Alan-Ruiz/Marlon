0401f2b966cc5c05c9f9bdad9a7d1beb67665fdc
Alan-Ruiz <gheryk90@gmail.com>
2021-07-19 18:39:11 +0200

    navbar changes display

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 
-  resources :users, only: [:show] do
-    resources :castings, only: [:show, :index, :new]
-  end
+
+ resources :castings, only: [:show, :index, :new]
+
 
   resources :posts, only: [:show, :create, :index]
 end
