d2a283caf70d1f592a473058d95bc950d66b2889
Alan-Ruiz <gheryk90@gmail.com>
2021-07-20 10:57:20 +0200

    dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
+  def dashboard
+    @user = User.find(params[:id])
+  end
+
   def home
-    @user = current_user
   end
 end
