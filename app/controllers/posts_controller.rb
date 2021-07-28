14ee6eb6c3c5072abceaa62295e04af1056f4dcf
Alan-Ruiz <gheryk90@gmail.com>
2021-07-28 13:07:58 +0200

    set posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,12 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  private
+
+  def set_casting
+    @casting = Casting.find(params[:id])
+  end
+
+
 end
